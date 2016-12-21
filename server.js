405148c22ea4be3bc459a69300498d14206acd90
M Dwikuntobayu <dwikunto.bayu@kiranatama.com>
2016-12-21 15:01:27 +0700

    working with express

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 var http = require('http'),
+    express = require('express'),
     faye = require('faye');
 
 var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
+var app = express();
+var server = http.createServer(app);
 // Handle non-Bayeux requests
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Faye server for Geeksfarm is running');
-});
+//var server = http.createServer(function(request, response) {
+//  response.writeHead(200, {'Content-Type': 'text/plain'});
+//  response.end('Faye server for Geeksfarm is running');
+//});
 bayeux.attach(server);
+
+bayeux.on('handshake', function(clientId) {
+    console.log('Client connected', clientId);
+});
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/app/index.html');
+});
+
+app.get('/user2', function(req, res) {
+    res.sendfile(__dirname + '/app/user2.html');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500);
+});
+
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 server.listen(8008);
 console.log("======================\nServer success running access in \n browser to http://localhost:8008 \nTo exit press 'ctr+c'\nNever Stop Learning Rocking ;)\n======================");
